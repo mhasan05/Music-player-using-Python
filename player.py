f66172b15e6a875bb135371adea448a3d248a8bf
teamerror <105315081+teamerror-net@users.noreply.github.com>
2023-01-17 19:34:32 +0600

    Add files via upload

---
+++ player.py
@@ -0,0 +1,43 @@
+# Music player using Python
+# https://www.teamerror.net
+
+from tkinter import *
+from tkinter import filedialog
+from pygame import mixer
+
+class MusicPlayer:
+    def __init__(self, window ):
+        window.geometry('320x120'); window.title('Music Player'); window.resizable(0,0)
+        Load = Button(window, text = 'Load',  width = 5, font = ('Times', 12), command = self.load)
+        Play = Button(window, text = 'Play',  width = 5,font = ('Times', 12), command = self.play)
+        Pause = Button(window,text = 'Pause',  width = 5, font = ('Times', 12), command = self.pause)
+        Stop = Button(window ,text = 'Stop',  width = 5, font = ('Times', 12), command = self.stop)
+        Load.place(x=30,y=20);Play.place(x=110,y=20);Pause.place(x=220,y=20);Stop.place(x=110,y=60) 
+        self.music_file = False
+        self.playing_state = False
+    def load(self):
+        self.music_file = filedialog.askopenfilename()
+    def play(self):
+        if self.music_file:
+            mixer.init()
+            mixer.music.load(self.music_file)
+            mixer.music.play()
+    def pause(self):
+        if not self.playing_state:
+            mixer.music.pause()
+            self.playing_state=True
+        else:
+            mixer.music.unpause()
+            self.playing_state = False
+    def stop(self):
+        mixer.music.stop()
+root = Tk()
+app= MusicPlayer(root)
+root.mainloop()
+
+
+
+
+
+
+
